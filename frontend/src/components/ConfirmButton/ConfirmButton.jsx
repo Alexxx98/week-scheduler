d9599c24224236d478e08fadaf6003801085b4a3
Aleksander Pres <aleksander.pres98@gmail.com>
2024-11-15 13:24:31 +0100

    login page done

---
+++ frontend/src/components/ConfirmButton/ConfirmButton.jsx
@@ -0,0 +1,9 @@
+import './ConfirmButton.css'
+
+function ConfirmButton(props) {
+    return (
+        <button>{props.text}</button>
+    )
+}
+
+export default ConfirmButton
--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,5 @@
 import Input from '../../components/Input/Input.jsx'
+import ConfirmButton from '../../components/ConfirmButton/ConfirmButton.jsx'
 import './LoginPage.css'
 
 function LoginPage() {
@@ -6,8 +7,11 @@ function LoginPage() {
         <>
             <div id="page-container">
                 <div id="login-form">
+                    <img src="/AppLogo.png" alt="AppLogo" />
+                    <h2>Login Form</h2>
                     <Input name="Name:" type="text" />
                     <Input name="Password:" type="password" />
+                    <ConfirmButton text="Login" />
                 </div>
             </div>
         </>
