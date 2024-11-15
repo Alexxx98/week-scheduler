65cb0e9e3b9427c96c9c7f37b40bd5844918408e
Aleksander Pres <aleksander.pres98@gmail.com>
2024-11-15 17:18:42 +0100

    registration page ui done

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,22 @@
-// import './RegisterPage.css'
+import Input from '../../components/Input/Input.jsx'
+import ConfirmButton from '../../components/ConfirmButton/ConfirmButton.jsx'
+import '../LoginPage/LoginPage.css'
 
 function RegisterPage() {
     return (
-        <h1>Register</h1>
+        <>
+            <div id="page-container">
+                <div id="login-form">
+                    <img src="/AppLogo.png" alt="AppLogo" />
+                    <h2>Registration Form</h2>
+                    <Input name="Email:" type="text" />
+                    <Input name="Name:" type="text" />
+                    <Input name="Password:" type="password" />
+                    <ConfirmButton text="Sign in" />
+                </div>
+            </div>
+        </>
     )
 }
 
-export default RegisterPage;
+export default RegisterPage
